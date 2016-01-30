70dac5f7d752caccb64a685b6267f252ff68642d
Paulofsr <paulofsr@yahoo.com.br>
2016-01-29 22:56:14 -0200

    Change .Net Framework

---
+++ ADMWeb/Controllers/ClienteController.cs
@@ -0,0 +1,26 @@
+﻿using SaucierLibrary.ClienteBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ADMWeb.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(Cliente.GetList());
+        }
+
+        public ActionResult Delete(string id)
+        {
+            if(!string.IsNullOrEmpty(id))
+            {
+                Cliente.Delete(new ClienteCriteriaBase(new Guid(id)));
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SaucierWeb/Controllers/HomeController - Copy.cs
@@ -0,0 +1,59 @@
+﻿using SaucierLibrary.ClienteBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SaucierWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+        }
+
+        public ActionResult Index()
+        {
+            //Cliente cliente = Cliente.Get(new ClienteCriteriaBase(new Guid("adfasdfasdfasdfasdf")));
+            //Geral.Logar("login", "OigaleaH1@3", new Guid("683BC06E-52EA-444A-97A6-F97E6FAC68C9"));
+            //Cliente cliente = Cliente.New(new ClienteCriteriaCreateBase());
+            //cliente.Nome = "Novo!! Again!!";
+            //cliente.Save();
+            //Usuario user = Usuario.New(new UsuarioCriteriaCreateBase(new Guid("B8F21383-8834-486C-98E4-2C09491565FD")));//cliente.Id));
+            //user.Email = "teste@teste";
+            //user.Nome = "Adm";
+            //user.EmailConfirmado = true;
+            //if (!user.SetLogin("login"))
+            //    throw new Exception("Login já existe.");
+            //user.Senha = "OigaleaH1@3";
+            //user.ConfirmarSenha = "OigaleaH1@3";
+            //user.Save();
+            //user = Usuario.ValidarLoginSenha("login", "OigaleaH1@3", new Guid("B8F21383-8834-486C-98E4-2C09491565FD"));
+            //if(user.Vazio)
+            //    user = Usuario.ValidarLoginSenha("login", "OigaleaH1", cliente.Id);
+
+            //kdjfhkajsdhf
+
+            //if (!Geral.Logado)
+            //    Geral.Logar("login", "OigaleaH1@3", new Guid("B8F21383-8834-486C-98E4-2C09491565FD"));
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
