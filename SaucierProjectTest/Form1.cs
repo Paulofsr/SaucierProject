000d66ea5790042070fbc696e2d52fd2f23dc85e
Paulofsr <paulofsr@yahoo.com.br>
2016-03-06 19:26:12 -0300

    Teste inicializado

---
+++ SaucierProjectTest/Form1.cs
@@ -0,0 +1,32 @@
+﻿using SaucierLibrary.ClienteBase;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaucierProjectTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Teste();
+        }
+
+        private void Teste()
+        {
+            Usuario user = Logar();
+        }
+
+        private Usuario Logar()
+        {
+            return Usuario.ValidarLoginSenha("login", "OigaleaH1@3", new Guid("B8F21383-8834-486C-98E4-2C09491565FD"));
+        }
+    }
+}
